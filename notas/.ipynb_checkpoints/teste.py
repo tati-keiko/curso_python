2f95deeb95b999b114ca2385232b92f6f19b915b
tati-keiko <tatianaumeda@gmail.com>
2022-04-03 21:41:24 -0300

    acrescentando notas

---
+++ notas/.ipynb_checkpoints/teste.py
@@ -0,0 +1,25 @@
+a = 2
+b = 3
+
+lista = [1, 2, 3]
+dicionario = {'nome': 'Leo', 'idade': 30}
+
+print('primeiro programa')
+
+print(2/3)
+
+x = 3
+y = x
+z = 3
+
+print(x is a)
+print(x is b)
+
+lista_a = [1, 2, 3]
+lista_b = lista_a
+lista_c = [1, 2, 3]
+print(lista_a is lista_b)
+print(lista_b is lista_c)
+
+lista_a[1] = 200
+lista_c[1] = 300

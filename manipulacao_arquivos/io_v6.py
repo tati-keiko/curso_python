1d79b9eac03dee2936eda9c46c83a66fbaf28ee7
tati-keiko <tatianaumeda@gmail.com>
2022-04-07 15:13:36 -0300

    Manipulação de arquivos

---
+++ manipulacao_arquivos/io_v6.py
@@ -0,0 +1,13 @@
+
+with open('pessoas.csv') as arquivo:
+    with open('pessoas.txt', 'w') as saida:
+        for registro in arquivo:
+            pessoa = registro.strip().split(',')
+            print('Nome: {}, Idade: {}'.format(*pessoa), file=saida)
+
+
+if arquivo.closed:
+    print('Arquivo já foi fechado!')
+
+if saida.closed:
+    print('Arquivo de saida já foi fechado!')

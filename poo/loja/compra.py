356994f2a1189b15970a5e13b24179920bf5afd4
tati-keiko <tatianaumeda@gmail.com>
2022-04-16 22:07:23 -0300

    POO parte 1

---
+++ poo/loja/compra.py
@@ -0,0 +1,6 @@
+class Compra:
+    def __init__(self, vendedor, cliente, data, valor):
+        self.vendedor = vendedor
+        self.cliente = cliente
+        self.data = data
+        self.valor = valor
